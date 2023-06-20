e9904adaeaab93b9e6eb2d128b75babc61e79d91
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-06-19 20:11:15 -0400

    navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo1} alt="logo" className='w-14 h-14 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Erkut Cakmak &nbsp;<span className='sm:block hidden font-light tracking-widest'>| Portfolio Website</span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Erkut Cakmak &nbsp;</p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
